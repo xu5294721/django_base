7f575bdf652d4b6827655bb4590e5d9cf44da40c
xu5294721 <xu5294721@gmail.com>
2022-05-20 15:27:07 +0800

    创建子应用

--- bookmanager/book/models.py
+++ bookmanager/book/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
+
+class BookInfo(models.Model):
+    # 主键自动生成，不需要单独填写
+    name = models.CharField(max_length=20)
+
+
+class PersonInfo(models.Model):
+    name = models.CharField(max_length=20)
+    gender = models.BooleanField()
