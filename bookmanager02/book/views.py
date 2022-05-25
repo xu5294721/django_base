636a57a29343636d844bd401b3d0306f2e8fd6a8
xu5294721 <xu5294721@gmail.com>
2022-05-25 10:38:28 +0800

    objects-增

--- bookmanager02/book/views.py
+++ bookmanager02/book/views.py
@@ -4,3 +4,13 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse('index')
+
+
+# objects--相当一个代理  实现增删改查
+from book.models import BookInfo
+
+BookInfo.objects.create(
+    name='测试开发简介',
+    pub_date='2010-01-01',
+    readcount=20,
+)
