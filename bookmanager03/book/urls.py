e6d023cc210ab6ee502b73978c324d427d4ba1b4
xu5294721 <xu5294721@gmail.com>
2022-05-31 16:59:38 +0800

    HttpRequest-form

--- bookmanager03/book/urls.py
+++ bookmanager03/book/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from book.views import create_book, shop
+from book.views import create_book, req_url, req_query, shop_02, register
 
 urlpatterns = [
     path('create/', create_book),
-    path('<city_id>/<shop_id>', shop)
+    path('<city_id>/<shop_id>/', req_url),
+    path('req_query/', req_query),
+    path('shop02/', shop_02),
+    path('register/', register),
+
 ]
--- bookmanager03/book/views.py
+++ bookmanager03/book/views.py
@@ -8,5 +8,37 @@ def create_book(request):
     return HttpResponse('create')
 
 
-def shop(request, city_id, shop_id):
+# HttpRequset对象：
+# 1.URL方式  截取URL特定部分，在服务器端的路由中用正则表达式截取。用于接收参数，向服务器传递数据
+def req_url(request, city_id, shop_id):
+    # 查询字符串方式获取
+    # query_params = request.GET
+    # print(query_params)
+    # order = query_params.getlist('order')
+    # print(order)
     return HttpResponse('君的饭店')
+
+
+# 2.查询字符串(query_string),例如/?key1=value&key2=values2
+def req_query(request):
+    query_params = request.GET
+    print(query_params)
+    order = query_params.getlist('order')
+    for i in order:
+        print(i)
+
+    return HttpResponse('req_query')
+
+
+def shop_02(request):
+    req_params = request.GET
+    order = req_params.get('id')
+    print(order)
+    return HttpResponse('shop_02')
+
+
+# 3.传递POST请求，
+def register(request):
+    req_post = request.POST
+    print(req_post)
+    return HttpResponse('register')
