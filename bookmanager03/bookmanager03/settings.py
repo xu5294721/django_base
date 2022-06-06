06b3ef65d2f19839207a17318026f3949671d7de
xu5294721 <xu5294721@gmail.com>
2022-06-06 15:32:53 +0800

    session-redis配置

--- bookmanager03/bookmanager03/settings.py
+++ bookmanager03/bookmanager03/settings.py
@@ -131,3 +131,17 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# redis数据库连接配置
+CACHES = {
+    'default': {
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': 'redis://127.0.0.1:6379/1',
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+        }
+    }
+}
+SESSION_ENGINE = 'django.contrib.sessions.backends.cache'
+SESSION_CACHE_ALIAS = 'default'
